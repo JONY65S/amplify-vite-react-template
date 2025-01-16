9a81d24f7d813c22e886464b6cfea1e2cff5544a
JONY65S <jonathan.baltazar85@outlook.com>
2025-01-16 11:08:46 -0600

    blank

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => (
-    <footer style={{ padding: "1rem", textAlign: "center", marginTop: "2rem" }}>
-      <p>&copy; {new Date().getFullYear()} InSightful</p>
-      <a href="/terms">Términos de Uso</a> | <a href="/privacy">Política de Privacidad</a>
-    </footer>
-  );
-  
-  export default Footer;
-  
